ddcb7ec4af11b073fc0d6ef73bbf57b1ca553533
Zhou Hao <zhouhao@fujitsu.com>
2025-04-13 16:34:11 +0800

    Add utils functions
    Signed-off-by: Zhou Hao <zhouhao@fujitsu.com>

--- internal/types/types.go
+++ internal/types/types.go
@@ -3,26 +3,39 @@ package types
 import v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 type ResourceClaimInfo struct {
-	Name              string   `json:"name"`
-	NodeName          string   `json:"node_name"`
-	CreationTimestamp v1.Time  `json:"creation_timestamp"`
-	Namespace         string   `json:"namespace"`
-	Devices           []Device `json:"devices"`
-	UsedByPod         bool     `json:"used_by_pod"`
+	Name              string                `json:"name"`
+	NodeName          string                `json:"node_name"`
+	CreationTimestamp v1.Time               `json:"creation_timestamp"`
+	Namespace         string                `json:"namespace"`
+	Devices           []ResourceClaimDevice `json:"devices"`
+	UsedByPod         bool                  `json:"used_by_pod"`
 }
 
-type Device struct {
+type ResourceClaimDevice struct {
 	Name  string `json:"name"`
 	Model string `json:"model"`
 	State string `json:"state"`
 }
 
 type ResourceSliceInfo struct {
+	Name              string                `json:"name"`
+	NodeName          string                `json:"node_name"`
+	CreationTimestamp v1.Time               `json:"creation_timestamp"`
+	Driver            string                `json:"driver"`
+	FabricID          string                `json:"fabric_id"`
+	FabricModel       string                `json:"fabric_model"`
+	Devices           []ResourceSliceDevice `json:"devices"`
+}
+
+type ResourceSliceDevice struct {
+	Name string `json:"name"`
+	UUID string `json:"uuid"`
 }
 
 type NodeInfo struct {
-	Name  string `json:"name"`
-	Model string `json:"model"`
-	Max   int    `json:"max"`
-	Min   int    `json:"min"`
+	Name        string `json:"name"`
+	FabricID    string `json:"fabric_id"`
+	FabricModel string `json:"fabric_model"`
+	Max         int    `json:"max"`
+	Min         int    `json:"min"`
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,11 +3,14 @@ package utils
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"time"
 
 	cdioperator "github.com/IBM/cdi-operator"
 	"github.com/InfraDDS/dynamic-device-scaler/internal/types"
 	resourceapi "k8s.io/api/resource/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -29,7 +32,7 @@ func GetResourceClaimInfo(ctx context.Context, kubeClient client.Client) ([]type
 		resourceClaimInfo.Namespace = rc.Namespace
 		resourceClaimInfo.CreationTimestamp = rc.ObjectMeta.CreationTimestamp
 		for _, device := range rc.Status.Devices {
-			var deviceInfo types.Device
+			var deviceInfo types.ResourceClaimDevice
 			deviceInfo.Name = device.Device
 			// waiting for PR https://github.com/kubernetes/kubernetes/pull/130160 to be merged
 			// Determine the state based on the added claim.status.Devices[].BindingConditions in https://github.com/kubernetes/kubernetes/pull/130160
@@ -46,6 +49,87 @@ func GetResourceClaimInfo(ctx context.Context, kubeClient client.Client) ([]type
 	}
 
 	return resourceClaimInfoList, nil
+}
+
+func GetResourceSliceInfo(ctx context.Context, kubeClient client.Client) ([]types.ResourceSliceInfo, error) {
+	var resourceSliceInfoList []types.ResourceSliceInfo
+
+	resourceSliceList := &resourceapi.ResourceSliceList{}
+	if err := kubeClient.List(ctx, resourceSliceList, &client.ListOptions{}); err != nil {
+		return nil, fmt.Errorf("failed to list ResourceClaims: %v", err)
+	}
+
+	for _, rs := range resourceSliceList.Items {
+		var resourceSliceInfo types.ResourceSliceInfo
+
+		resourceSliceInfo.Name = rs.Name
+		resourceSliceInfo.CreationTimestamp = rs.CreationTimestamp
+		resourceSliceInfo.Driver = rs.Spec.Driver
+		resourceSliceInfo.NodeName = rs.Spec.NodeName
+
+		if rs.Spec.NodeSelector != nil {
+			for _, term := range rs.Spec.NodeSelector.NodeSelectorTerms {
+				for _, expr := range term.MatchExpressions {
+					switch expr.Key {
+					case "fabric":
+						if len(expr.Values) > 0 {
+							resourceSliceInfo.FabricID = expr.Values[0]
+						}
+						//TODO: get more information from the fabric
+					}
+				}
+			}
+		}
+
+		for _, device := range rs.Spec.Devices {
+			if device.Basic != nil {
+				var deviceInfo types.ResourceSliceDevice
+				deviceInfo.Name = device.Name
+				for attrName, attrValue := range device.Basic.Attributes {
+					if attrName == "uuid" {
+						deviceInfo.UUID = attrValue.String()
+					}
+				}
+				resourceSliceInfo.Devices = append(resourceSliceInfo.Devices, deviceInfo)
+			}
+		}
+	}
+
+	return resourceSliceInfoList, nil
+}
+
+func GetNodeInfo(ctx context.Context, clientSet *kubernetes.Clientset) ([]types.NodeInfo, error) {
+	var nodeInfoList []types.NodeInfo
+
+	nodes, err := clientSet.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list Nodes: %v", err)
+	}
+
+	for _, node := range nodes.Items {
+		var nodeInfo types.NodeInfo
+
+		nodeInfo.Name = node.Name
+
+		// TODO: get label-prefix from ConfigMap
+		fabricIDValue, exists := node.Labels["composable.test/fabric"]
+		if exists {
+			nodeInfo.FabricID = fabricIDValue
+		}
+
+		MaxValue, exists := node.Labels["composable.test/nvidia-a100-80g-siz-max"]
+		if exists {
+			nodeInfo.Max, _ = strconv.Atoi(MaxValue)
+		}
+
+		MinValue, exists := node.Labels["composable.test/nvidia-a100-80g-siz-min"]
+		if exists {
+			nodeInfo.Min, _ = strconv.Atoi(MinValue)
+		}
+
+	}
+
+	return nodeInfoList, nil
 }
 
 func UpdateComposableResourceLastUsedTime(ctx context.Context, kubeClient client.Client) error {
@@ -82,4 +166,8 @@ func UpdateComposableResourceLastUsedTime(ctx context.Context, kubeClient client
 func IsDeviceUsed(uuid string) (bool, error) {
 
 	return false, nil
+}
+
+func isDeviceAvailable() bool {
+	return true
 }
