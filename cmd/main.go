8fc1f3433b23d280c2e563d9d691424e12f1049a
Zhou Hao <zhouhao@fujitsu.com>
2025-07-04 11:20:27 +0800

    fix domain
    Signed-off-by: Zhou Hao <zhouhao@fujitsu.com>

--- cmd/main.go
+++ cmd/main.go
@@ -89,7 +89,7 @@ func main() {
 		Scheme:                 scheme,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "2313b367.infra.dds",
+		LeaderElectionID:       "2313b367.co.hdi",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
